e658bf6049045f71af68265841cbfd5011577e81
Efield44444235364 <efi3lx@gmail.com>
2025-08-25 19:54:44 +0700

    Create BF.lua

---
+++ BF.lua
@@ -0,0 +1,12 @@
+for i, v in pairs(getgc(true)) do
+    if typeof(v) == "function" and islclosure(v) then
+        local upvs = debug.getupvalues(v)
+        for _, uv in pairs(upvs) do
+            if uv == "AndroidAnticheatKick" then
+                hookfunction(v, function(...)
+                    return nil
+                end)
+            end
+        end
+    end
+end
