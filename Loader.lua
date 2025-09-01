62c88fa95e7a9d0fea1debe262f09fb19cb4964e
Efield44444235364 <efi3lx@gmail.com>
2025-09-01 08:50:29 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -13,9 +13,11 @@ local function loadingString(name, url)
 end
 
 --// Files
-loadingString("Ui", "https://raw.githubusercontent.com/Efield44444235364/Ban/refs/heads/main/Ui.lua")
-loadingString("Auto Reconnect", "https://raw.githubusercontent.com/Efield44444235364/Ban/refs/heads/main/Auto%20reconnect.lua")
 loadingString("BF", "https://raw.githubusercontent.com/Efield44444235364/Ban/refs/heads/main/BF.lua")
 loadingString("Admins", "https://raw.githubusercontent.com/Efield44444235364/Ban/refs/heads/main/Admins.lua")
 loadingString("Oni optimize", "https://raw.githubusercontent.com/Efield44444235364/Ban/refs/heads/main/Oni.lua")
+loadingString("Ui", "https://raw.githubusercontent.com/Efield44444235364/Ban/refs/heads/main/Ui.lua")
+loadingString("Auto Reconnect", "https://raw.githubusercontent.com/Efield44444235364/Ban/refs/heads/main/Auto%20reconnect.lua")
+
+
 
