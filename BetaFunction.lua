eb803e034034c87597433954f1383667bc69d473
Efield44444235364 <efi3lx@gmail.com>
2025-09-28 15:13:28 +0700

    Update BetaFunction.lua

--- BetaFunction.lua
+++ BetaFunction.lua
@@ -1,7 +1,7 @@
 local StarterGui = game:GetService("StarterGui")
 
 -- ===== World Check =====
-local World1, World2, World3 = false, false, false
+local World1, World2, World3, Celebrity = false, false, false, false
 
 if game.PlaceId == 2753915549 then
     World1 = true
@@ -9,6 +9,8 @@ elseif game.PlaceId == 4442272183 then
     World2 = true
 elseif game.PlaceId == 7449423635 then
     World3 = true
+elseif game.PlaceId == 95165932064349 then 
+    Celebrity= true
 else
     warn("[❌] This script only works in Blox Fruits PlaceIds!")
     return
