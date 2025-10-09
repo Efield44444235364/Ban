501310a5696ec00216439129239919a020a753ec
Efield44444235364 <efi3lx@gmail.com>
2025-10-09 16:36:53 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,5 @@
+task.wait(2)
+
 if not _G.efield_loader then
     game.Players.LocalPlayer:Kick("This is a fucking old script pls get new from Dev!!!!. End of story🥰😘 \n in 4sec game gonnabe shutdown!")
     wait(4)
